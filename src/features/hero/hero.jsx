9adf9ca3c6a7be955156624f86b5c14406d7a160
ldoughty <luke.doughty@me.com>
2026-04-19 15:26:51 -0500

    Updated resume and projects

--- src/features/hero/hero.jsx
+++ src/features/hero/hero.jsx
@@ -49,7 +49,7 @@ export function Hero() {
         </div>
         <div className={styles.links}>
           <button onClick={panDownOnClick}>&gt; PROJECTS</button>
-          <a href="https://lukedoughty.me/pdfs/Term5ResumeEmbeddedLaTeX.pdf">&gt; RESUME</a>
+          <a href="https://lukedoughty.me/pdfs/Term6_5ResumeEmbeddedLaTeX.pdf">&gt; RESUME</a>
           <a href="https://www.linkedin.com/in/lukedoughty/">&gt; LINKEDIN</a>
         </div>
       </div>
